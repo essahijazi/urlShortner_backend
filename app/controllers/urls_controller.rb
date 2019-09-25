7a81350d5eff6737e43f2ba65e8faffff836aba3
Essa Hijazi <essahijazi18@gmail.com>
2019-09-25 18:25:39 -0400

    fixed the shorten action

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,17 +1,45 @@
 class UrlsController < ApplicationController
-
-  def show
-  end
+  skip_before_action :verify_authenticity_token
 
   def shorten
-    url = params[:originalURL]
-    render json: {url: url}
+
+    # original_url = params[:original_url]
+    # shortened = Url.where(["original_url=?", original_url]).first
+
+    #checks if url has already been shortened
+    shortened = Url.where(["original_url=?", params[:original_url]]).first
+    
+
+    if shortened
+      #url HAS been shortened before, short url is returned
+      render json: {short_url: shortened.short_url}
+    else  
+      #url HAS NOT been shortened before
+
+      #create a new url instance so i can use it's id
+      new_shortened = Url.new(original_url: params[:original_url])
+      new_shortened.save
+
+      #use the url id and convert it into base 85 number
+      base85_id = Url.convert_id_to_base85(new_shortened.id)
+      
+      #convert base 85 number into a unique string
+      new_short_url = Url.convert_base85_to_new_url(base85_id)
+
+      #add the new short url to the url instane that was created
+      new_shortened.short_url = new_short_url
+      new_shortened.save
+
+      #short url of new url instance is returned
+      render json: {short_url: new_shortened.short_url}
+
+    end
   end
 
   private
 
-  def shorten_url_helper(url)
-    
+  def sho
+
   end
 
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,20 +1,25 @@
 class Url < ApplicationRecord
-    
-    def convert_id_to_base85(url_id)
+        
+    def self.convert_id_to_base85(url_id)
         url_id = url_id.to_i
         str = ""
         
+        #convert the id from base 10 to base 85 using the division and modulus method
+        #placing space between each base 85 number for separation and clarity purposes
         while (url_id > 0)
             str += (url_id % 85).to_s
             str += " "
             url_id /= 85
         end
         
+        #string needs to be reverse after using division and modulus conversion method
+        #.strip is used to remove the leading whitespace after the string is reversed
         return str.reverse.strip
     end
 
-    def convert_base85_to_new_url(base85_num)
+    def self.convert_base85_to_new_url(base85_num)
 
+        #array of permissable url characters(total = 85 characters, hence base 85)
         url_characters = [
                 "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p","q",
                 "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H",
@@ -25,9 +30,9 @@ class Url < ApplicationRecord
         base_url = "sho.rt/"
         str = ""
 
-        base85_array = base85_num.split(" ")
-        puts base85_array[0];
-        
+        #splits the series of base 85 numbers into individual numbers
+        #each base 85 number in the array will reperesent the index of 
+        #the character that will used to create str
         base85_num.split(" ").each do |n|
             str += url_characters[n.to_i].to_s
         end
